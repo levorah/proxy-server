c4adc527d332e057e39adcba26141ad673a57b3f
levorah <levora004@gmail.com>
2025-03-04 11:52:04 +0000

    fix: issue regarding queryParams need to check whether it is resolved or not

--- src/server.js
+++ src/server.js
@@ -12,13 +12,15 @@ app.use(express.urlencoded({ extended: true }))
 
 app.get('/fetch/*', async (req, res) => {
     const targetUrl = req.params[0]
+    const queryParams = new URLSearchParams(req.query).toString(); // Forward all query parameters
+
     if (!targetUrl) {
         return res.status(400).json({ error: "Missing target URL" })
     }
     try {
-        const response = await axios.get(targetUrl, {
-            headers: { ...req.headers, host: new URL(targetUrl).host }
-        })
+        const response = await axios.get(`${targetUrl}?${queryParams}`, {
+            headers: { ...req.headers, host: new URL(targetUrl).host },
+        });
         res.set(response.headers)
         res.status(response.status).send(response.data)
     } catch (error) {
